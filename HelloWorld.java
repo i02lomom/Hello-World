eeb7585ba7f005cf78c2e100c50400ebd0d89b98
Miguel Ángel López <i02lomom@gmail.com>
2015-11-05 19:20:40 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+* Prints "Hello world" to the terminal window.
+* @author Miguel Angel Lopez Moyano
+* @version 1.0
+*/
+public class HelloWorld {
+	public static void main(String [] args){
+		System.out.println("\nHello world");
+	}
+}
