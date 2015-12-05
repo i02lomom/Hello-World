f4eaac4482aab11e9f142c519f362a21f58efdf9
Miguel Ángel López Moyano <i02lomom@gmail.com>
2015-12-05 14:10:57 +0100

    Create HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+/**
+ * Imprime por pantalla el mensaje Hola mundo!
+ * 
+ * @author Miguel Angel López Moyano
+ * @version 1.0
+ */
+public class HolaMundo{
+	public static void main(String[] args){
+		System.out.println("Hola mundo!");
+		System.out.println("Soy Miguel Angel Lopez");
+	}
+}
